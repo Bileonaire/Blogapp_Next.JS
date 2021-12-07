78a087f4e392d3b50cc6dd78585b18b0bd3f3397
Bileonaire <kiokoleon@gmail.com>
2021-12-07 11:46:07 +0300

    Blog App setup

---
+++ components/Categories.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+
+const Categories = () => {
+    return (
+        <div>
+            categories
+        </div>
+    )
+}
+
+export default Categories
