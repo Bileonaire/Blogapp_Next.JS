101e25499aa07443299d921e896297abe7baab7b
Bileonaire <kiokoleon@gmail.com>
2021-12-07 17:32:55 +0300

    Create Dynamic Pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components';
+import { FeaturedPosts } from '../sections';
 import { getPosts } from '../services';
 
 
@@ -10,6 +11,7 @@ export default function Home({ posts }) {
         <title>CMS BLOG</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (<PostCard post={post.node} key={index}/>))}
